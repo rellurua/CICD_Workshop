a42a289bfe3787cc39d7df07c22ab7c870cc3801
“rellurua” <relluru@amazon.com>
2025-10-01 02:20:34 +0000

    Added Docker build and push

--- app-cdk/app.py
+++ app-cdk/app.py
@@ -1,10 +1,16 @@
+
+
+
+
 #!/usr/bin/env python3
 import os
+
 
 import aws_cdk as cdk
 
 from app_cdk.app_cdk_stack import AppCdkStack
 from app_cdk.pipeline_cdk_stack import PipelineCdkStack
+from app_cdk.ecr_cdk_stack import EcrCdkStack
 
 app = cdk.App()
 
@@ -13,9 +19,15 @@ app_stack = AppCdkStack(
     'app-stack'
 )
 
+ecr_stack = EcrCdkStack(
+    app,
+    'ecr-stack'
+)
+
 pipeline_stack = PipelineCdkStack(
     app,
     'pipeline-stack',
+    ecr_repository = ecr_stack.ecr_data,
 )
 
 app.synth()
--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -1,3 +1,4 @@
+import os
 from constructs import Construct
 from aws_cdk import (
     Stack,
@@ -6,11 +7,11 @@ from aws_cdk import (
     aws_codepipeline as codepipeline,
     aws_codebuild as codebuild,
     aws_codepipeline_actions as codepipeline_actions,
+    aws_iam as iam,
 )
 
 class PipelineCdkStack(Stack):
-
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, ecr_repository, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Creates a CodeConnections resource called 'CICD_Workshop_Connection'
@@ -35,9 +36,52 @@ class PipelineCdkStack(Stack):
                 compute_type = codebuild.ComputeType.LARGE
             ),
         )
+        docker_build_project = codebuild.PipelineProject(
+            self, 'Docker Build',
+            build_spec = codebuild.BuildSpec.from_source_filename('./buildspec_docker.yml'),
+            environment = codebuild.BuildEnvironment(
+                build_image = codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged = True,
+                compute_type = codebuild.ComputeType.LARGE,
+                environment_variables = {
+                    'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = 'latest'
+                    ),
+                    'IMAGE_REPO_URI': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = ecr_repository.repository_uri
+                    ),
+                    'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = os.environ['CDK_DEFAULT_REGION']
+                    )
+                }
+            ),
+        )
+
+        docker_build_project.add_to_role_policy(iam.PolicyStatement(
+            effect = iam.Effect.ALLOW,
+            actions = [
+                'ecr:GetAuthorizationToken',
+                'ecr:BatchCheckLayerAvailability',
+                'ecr:GetDownloadUrlForLayer',
+                'ecr:GetRepositoryPolicy',
+                'ecr:DescribeRepositories',
+                'ecr:ListImages',
+                'ecr:DescribeImages',
+                'ecr:BatchGetImage',
+                'ecr:InitiateLayerUpload',
+                'ecr:UploadLayerPart',
+                'ecr:CompleteLayerUpload',
+                'ecr:PutImage'
+            ],
+            resources = ['*'],
+        ))
 
         source_output = codepipeline.Artifact()
         unit_test_output = codepipeline.Artifact()
+        docker_build_output = codepipeline.Artifact()
 
         source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
           action_name = 'GitHub',
@@ -65,7 +109,17 @@ class PipelineCdkStack(Stack):
             stage_name = 'Code-Quality-Testing',
             actions = [build_action]
         )
+        docker_build_action = codepipeline_actions.CodeBuildAction(
+            action_name = 'Docker-Build',
+            project = docker_build_project,
+            input = source_output,
+            outputs = [docker_build_output]
+        )
 
+        pipeline.add_stage(
+            stage_name = 'Docker-Push-ECR',
+            actions = [docker_build_action]
+        )
         CfnOutput(
             self, 'SourceConnectionArn',
             value = SourceConnection.attr_connection_arn
